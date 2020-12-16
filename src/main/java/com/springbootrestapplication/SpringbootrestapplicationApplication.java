a01d9531ab1275f282f7bf2ceb0427bbe2a1fbb0
Gokul <gokulcode234@gmail.com>
2020-12-16 10:57:45 +0530

    Initital Commit

---
+++ src/main/java/com/springbootrestapplication/SpringbootrestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootrestapplication/SpringbootrestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
